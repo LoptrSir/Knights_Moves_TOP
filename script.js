15c29e3319f0799281fc4faafb207fe472895a66
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2024-07-15 15:19:30 -0600

    info.txt 7/15

---
+++ script.js
@@ -0,0 +1,85 @@
+//Knights Move TOP
+
+//Steps: create graph array, create adjacent list,
+
+//KnightsMove JS
+class Grid {
+  constructor() {
+    this._size = 8;
+    this._gridArray = [];
+    // this._adjacentList = [];
+    this._adjacentList = {};
+  }
+
+  makeGraph() {
+    for (let i = 0; i < this._size; i++) {
+      this._gridArray[i] = [];
+      for (let j = 0; j < this._size; j++) {
+        let value = [i, j];
+        this._gridArray[i][j] = value;
+      }
+    }
+    console.log("makeGraph:", this._gridArray);
+    this.makeAdjacentList(this._gridArray);
+    console.log("AdjacentList:", this._adjacentList);
+  }
+
+  makeAdjacentList(_gridArray) {
+    for (let i = 0; i < _gridArray.length; i++) {
+      for (let j = 0; j < _gridArray.length; j++) {
+        let cell = _gridArray[i][j];
+        this.testMoves(cell);
+      }
+    }
+  }
+
+  testMoves(cell) {
+    //Do I need a reference to the original cell?
+    let [i, j] = cell;
+    const moves = [
+      [i + 2, j + 1],
+      [i + 2, j - 1],
+      [i - 2, j + 1],
+      [i - 2, j - 1],
+      [i + 1, j + 2],
+      [i + 1, j - 2],
+      [i - 1, j + 2],
+      [i - 1, j - 2],
+    ];
+
+    this._adjacentList[cell] = [];
+
+    for (let [newI, newJ] of moves) {
+      if (this.isValidMove(newI, newJ)) {
+        // this._adjacentList.push([newI, newJ]);
+        this._adjacentList[cell].push([newI, newJ]);
+      }
+    }
+  }
+
+  isValidMove(i, j) {
+    // if (i >= 0 && i < this._size && j >= 0 && j < this._size) {
+    //   return true;
+    // }
+    // return false;
+   return i >= 0 && i < this._size && j >= 0 && j < this._size
+
+  }
+}
+
+function myFooter() {
+  const footer = document.querySelector(".footer");
+  footer.style.backgroundColor = "#333";
+  footer.style.fontSize = "1rem";
+  footer.style.color = "#f8afe5";
+  footer.style.padding = "3px";
+  footer.style.textAlign = "center";
+  footer.style.position = "fixed";
+  footer.style.width = "100%";
+  footer.style.bottom = "0";
+  footer.innerHTML = "LoptrSir";
+}
+myFooter();
+
+const testGrid = new Grid();
+testGrid.makeGraph();
